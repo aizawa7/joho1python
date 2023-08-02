88716d976561dee1c1727f6e67447a51035bc8a6
aizawa7 <mo2.null777@gmail.com>
2023-08-02 10:37:36 +0900

    これからバブルソートやります

---
+++ kadai5.9 1からその数まで入力.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = 0
+for q in range(a):
+    b = b + 1
+    print(b)
---
+++ kadai6 1から足し算.py
@@ -0,0 +1,7 @@
+a = 0
+b = 0
+c = int(input())
+for q in range(c):
+    a = a + 1
+    b = b + a
+    print(b)
